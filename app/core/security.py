56e60dc8a54031e6d6f0943d0e62c5d50ff5c154
jigordev <jigordev@gmail.com>
2025-04-20 00:23:54 -0300

    feat: using scutum extension auth configuration

--- app/core/security.py
+++ app/core/security.py
@@ -1,18 +1,19 @@
-from scutum import Gate
-from jose import JWSError, jwt
+from jwt import decode, InvalidTokenError
+from scutum.ext.fastapi import auth_config
 from fastapi import Security, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from app.core.config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-gate = Gate()
 
 def get_current_user(token: str = Security(oauth2_scheme)):
     try:
-        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.jwt_algorithm])
+        payload = decode(token, settings.secret_key, algorithms=[settings.jwt_algorithm])
         username: str = payload.get("sub")
         if username is None:
             raise HTTPException(status_code=401, detail="Token inválido")
         return {"username": username}
-    except JWSError:
+    except InvalidTokenError:
         raise HTTPException(status_code=401, detail="Token inválido")
+    
+auth_config.get_current_user = get_current_user
