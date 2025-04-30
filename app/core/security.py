0000c8ffc19780233a2cde86efd26f1c32b6234f
jigordev <jigordev@gmail.com>
2025-04-30 19:18:29 -0300

    feat: update scutum dependency

--- app/core/security.py
+++ app/core/security.py
@@ -1,8 +1,10 @@
 from jwt import decode, InvalidTokenError
-from scutum.ext.fastapi import auth_config
+from scutum.ext.fastapi import create_api_gate
 from fastapi import Security, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from app.core.config import settings
+from app.policies.user import UserPolicy
+from app.policies.post import PostPolicy
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -15,5 +17,7 @@ def get_current_user(token: str = Security(oauth2_scheme)):
         return {"username": username}
     except InvalidTokenError:
         raise HTTPException(status_code=401, detail="Token inválido")
-    
-auth_config.get_current_user = get_current_user
+
+gate = create_api_gate(get_current_user)
+gate.add_policy("users", UserPolicy)
+gate.add_policy("posts", PostPolicy)
