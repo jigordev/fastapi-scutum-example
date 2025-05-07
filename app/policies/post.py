ca16d418c7445292654f9a8ef4f356f9a1b2563a
jigordev <jigordev@gmail.com>
2025-05-06 21:27:12 -0300

    refactor: using policy decorator

--- app/policies/post.py
+++ app/policies/post.py
@@ -1,5 +1,7 @@
 from scutum import AsyncPolicy
+from app.core.security import gate
 
+@gate.policy("posts")
 class PostPolicy(AsyncPolicy):
     async def view(self, user, post):
         return True
--- app/policies/user.py
+++ app/policies/user.py
@@ -1,5 +1,7 @@
 from scutum import AsyncPolicy
+from app.core.security import gate
 
+@gate.policy("users")
 class UserPolicy(AsyncPolicy):
     async def view(self, user):
         return user.role == "admin"
